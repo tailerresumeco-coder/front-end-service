5a13e5adf6531616e5ecbe626828f43e8ad60c39
Ranadeep Bashetty <ranadeepbashetty008@gmail.com>
2026-04-12 23:34:12 +0530

    oauth2 google

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Login.css";
 import { useNavigate } from "react-router-dom";
-import { login } from "../services/resumeService";
+import { login, OAuthGoogleSignup } from "../services/resumeService";
 
 const Login = () => {
+    const googleButtonRef = useRef(null); // Ref for the Google button
+    
     const navigate = useNavigate();
     const [form, setForm] = useState({ email: "", password: "" });
 
@@ -15,7 +17,6 @@ const Login = () => {
         e.preventDefault();
         try {
             const res = await login(form);
-            console.log(res);
             if (res.status === 200) {
                 localStorage.setItem("access_token", res.data.access_token);
                 navigate("/");
@@ -24,6 +25,36 @@ const Login = () => {
             console.log(error);
         }
     };
+
+
+    const handleCallbackResponse = async (obj) => {
+        const res = await OAuthGoogleSignup({ credential: obj.credential });
+        if (res && res?.data?.access_token) {
+            localStorage.setItem("access_token", res?.data?.access_token);
+            navigate("/");
+        }
+    };
+
+    useEffect(() => {
+        /* global google */
+        // Initialize Google SDK
+        if (window.google) {
+            google.accounts.id.initialize({
+                client_id: "918373638000-sftdttmalfbclcnk8g9ss3fnk7pm8gg4.apps.googleusercontent.com",
+                callback: handleCallbackResponse,
+            });
+
+            // Render the official button into our ref
+            google.accounts.id.renderButton(googleButtonRef.current, {
+                theme: "outline",
+                size: "ams",
+                text: "continue_with", // Displays "Sign up with Google"
+                shape: "rectangular",
+                width: "220", // Adjust to match your form width
+            });
+        }
+    }, []);
+
 
     return (
         <div className="auth-container">
@@ -54,6 +85,9 @@ const Login = () => {
                 <p className="switch">
                     Don’t have an account? <span onClick={() => navigate("/sign-up")}>Sign up</span>
                 </p>
+                <div className="google-signup-wrapper" style={{ marginTop: "20px", display: "flex", justifyContent: "center" }}>
+                    <div ref={googleButtonRef}></div>
+                </div>
             </div>
         </div>
     );
